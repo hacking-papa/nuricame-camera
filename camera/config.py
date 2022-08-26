2cb6aba4b53402d902a6d0859c175455044ef182
SHINICHIRO SUZUKI <shin@sforzando.co.jp>
2022-08-26 17:40:36 +0900

    feat: :+1: set mac address

--- camera/config.py
+++ camera/config.py
@@ -4,5 +4,5 @@ config = configparser.ConfigParser()
 
 config["DEFAULT"] = {
     "server_url": "http://192.168.100.9:8000/",
-    "printer_mac_address": "00:00:00:00:00:00",
+    "printer_mac_address": "00:15:83:B7:0E:91",
 }
