02742ca76d47bfb5c92c9ff65bebe18a9b0e689d
SHINICHIRO SUZUKI <shin@sforzando.co.jp>
2022-08-28 15:39:22 +0900

    fix: :bug: typo

--- camera/uploader.py
+++ camera/uploader.py
@@ -7,7 +7,7 @@ from PIL import Image
 
 class Uploader:
     def upload(self, upload_filename: str, output_filename: str = "output.jpg"):
-        server_url = config.get("DEFAULT", "server_server_url")
+        server_url = config.get("DEFAULT", "server_url")
         file = {"file": open(upload_filename, mode="rb")}
         response = requests.post(server_url, files=file)
         print(response)
